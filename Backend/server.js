cd154ac5142cbc24a955ea36da48c008fd05dbcb
PigZahHot <coliva358@gmail.com>
2026-03-12 08:54:09 +0800

    Schema

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 
 const notesRoutes = require('./routes/notes.js');
 
 // Create an instance of the Express application
 const app = express();
-
-// Use the noutes routes
-app.use('/api/notes', notesRoutes);
 
 // Middleware
 app.use(express.json());
@@ -17,8 +15,13 @@ app.use((req, res, next) => {
     next();
 });
 
-// Remove Basic route to test the server
+// Use the noutes routes
+app.use('/api/notes', notesRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log('Server and Database are running on port', process.env.PORT);
+    })
+}).catch((error) => {
+    console.error('Error connecting to MongoDB: ', error)
+})
