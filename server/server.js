37f1b1ae54213f61a520c72f2daca9726ed536d1
Matthew Jompilla <mcjompilla27@gmail.com>
2026-03-12 08:05:34 +0800

    Mongo setup
    - Added connection to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 
 const express = require('express');
+
+//MongoDB require
+const mongoose = require('mongoose');
+
+//Routing
 const ToNotes = require('./routes/notes');
 const app = express();
 
@@ -11,13 +16,19 @@ app.use((req, res, next) => {
 
 app.use('/api/notes', ToNotes);
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('Server is running on port', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.error('OY MAY ERROR KA:', error);
+});
+
+
 app.use(express.json());
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
-
-app.listen(process.env.PORT, () => {
-    console.log('Server is running on port', process.env.PORT);
-});
-
