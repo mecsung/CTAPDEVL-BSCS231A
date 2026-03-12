965f340848e347335021f0211358ea32c5799ee7
kyleparas47-beep <kyleparas47@gmail.com>
2026-03-12 09:04:33 +0800

    updated

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,14 +1,35 @@
-const express = require('express');
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    //    res.send('Hello World');
-    res.json({
-        message: 'Hello World',
-        name: 'John Doe',
-        age: 67
-    });
-})
+    res.json({message: 'Welcome to the Notes API!'});
+});
+ 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({message: `You requested note with ID: ${id}`});
+});
+ 
+router.post('/', async (req, res) => {
+    const {title, content } =req.body;
 
-module.exports = router;
+    try{
+        const note = await Note.create({ title, content });
+        res.status(200).json(note);
+    }
+    catch (error){
+        res.status(400).json({error: message});
+
+    }
+    res.json({message: 'Note created successfully!'});
+});
+ 
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({message: `Note with ID: ${id} deleted successfully!`});
+});
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({message: `Note with ID: ${id} updated successfully!`});
+});
+
+ module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,20 @@
 require('dotenv').config();
-const express = require('express')
+
+const express = require('express');
+const mongoose =require('mongoose');
 
 const notesRouter = require('./routes/notes');
 const app = express();
-
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
-
+// ROUTE TO NOTES
 app.use('/api/notes', notesRouter);
-
-app.listen(process.env.PORT, () => [
-    console.log('Server is running on port 3000!!!', process.env.PORT)
-]);
+mongoose.connect(process.env.MONGO_URI).then(() =>{
+    app.listen(process.env.port, () => {
+        console.log('Server and database are running on port', process.env.port);
+    })
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
