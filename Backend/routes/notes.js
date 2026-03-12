8410475b618942fcdcfc6e0ce29956168712e928
Krisha Agojo <krishaagojo07@gmail.com>
2026-03-12 08:59:36 +0800

    Session 2
    - Server
    - NotesModel
    - Update POST Method

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,14 +1,44 @@
 const express = require('express');
-
+const Note = require('../models/noteModel')
+ 
 const router = express.Router();
-
+ 
+//GET notes
 router.get('/', (req, res) => {
-    //    res.send('Hello World');
-    res.json({
-        message: 'Hello World',
-        name: 'John Doe',
-        age: 67
-    });
-})
-
+    res.json({ message: 'Hello world' });
+});
+ 
+//GET ID
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ message: `Usto ko ng ${id} na itlog` });
+});
+ 
+//POST
+router.post('/', async (req, res) => {
+    const { title, content } = req.body;
+ 
+    try{
+        const note = await Note.create({title, content });
+        res.status(200).json(note);
+    }
+    catch(error){
+        res.status(400).json({error: error.message});
+    }
+    res.json({message: "May note ka na"})
+});
+ 
+//UPDATE
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ message: `Id #${id} is updated` });
+});
+ 
+//DELETE
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ message: `Sorry stict ang parents ko ${id} ` });
+});
+ 
+ 
 module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 
 const notesRouter = require('./routes/notes');
 const app = express();
@@ -9,8 +10,14 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.json());
 app.use('/api/notes', notesRouter);
 
-app.listen(process.env.PORT, () => [
-    console.log('Server is running on port 3000!!!', process.env.PORT)
-]);
+//connect MongoDB and start the server
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log('Server and Database are running on port', process.env.PORT);
+    })
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error)
+});
