726457243778bea4f246e1e8fe0ca13a62e2e13d
Caleb <calebhopemanalo@outlook.com>
2026-03-12 08:03:39 +0800

    Added Main Files

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -2,13 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
+// Define your routes here
 router.get('/', (req, res) => {
-    //    res.send('Hello World');
-    res.json({
-        message: 'Hello World',
-        name: 'John Doe',
-        age: 67
-    });
+    res.send('Notes route');
+});
+
+//Get single note
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    res.json({ message: `requested note with ID: ${id}`});
+});
+
+//Create a new note
+router.post('/', (req, res) => {
+    res.json({ message: 'Note created successfully' });
+});
+
+//Post create a new note
+router.post('/', (req, res) => {
+    res.json({ message: 'Note created successfully' });
+});
+
+//Delete a note
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    res.json({ message: `Note with ID: ${id} deleted successfully` });
+});
+
+//Update a note
+router.patch('/:id', (req, res) => {
+    const {id} = req.params;
+    res.json({ message: `Note with ID: ${id} updated successfully` });
 })
 
 module.exports = router;
