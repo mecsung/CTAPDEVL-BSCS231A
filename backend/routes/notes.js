6ceb392a7d5099a21684bce8fff3ccce43213408
justhMae <maetuiza9@gmail.com>
2026-03-12 08:54:24 +0800

    Mongo Setup
    - setting up MongoDB using mongoose

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Note = require('../models/noteModel');
 
 const router = express.Router();
 
@@ -11,8 +12,21 @@ router.get('/:id', (req, res) => {
   res.json({ message: 'You requested note with ID: ${id}' });
 });
 
-router.post('/', (req, res) => {
-  res.json({ message: 'Note created successfully!' });
+// router.post('/', (req, res) => {
+//   res.json({ message: 'Note created successfully!' });
+// });
+
+router.post('/', async (req, res) => {
+  const { title, content } = req.body;
+
+  try {
+      const note = await Note.create({title, content});
+      res.status(200).json(note);
+  }
+  catch(error) {
+      res.status(400).json({error: error.message});
+  }
+  res.json({message: 'Note created successfully!'});
 });
 
 router.delete('/:id', (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,42 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 
 const notesRouter = require('./routes/notes');  
 
 const app = express();
+
+//need to be in middle
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();   
 });
 
+app.use('/api/notes', notesRouter);
+
 // app.get('/', (req, res) => {
 //   res.send('Hello World!');
 // });
 
-app.listen(process.env.PORT, () => {
-  console.log('Server is running on port', process.env.PORT);
-});
+mongoose. connect (process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('Server and Database are running on port', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+// app.listen(process.env.PORT, () => {
+//   console.log('Server is running on port', process.env.PORT);
+// });
+
 
