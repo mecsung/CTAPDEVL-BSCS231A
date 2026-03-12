085996f932d408707b13aab8b78b2ed1967f72bc
Christian <115513703+CalChrisTian@users.noreply.github.com>
2026-03-12 08:53:44 +0800

    Update Files

---
+++ backend/models/noteModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+
+}, {timestamp: true}
+
+);
+
+module.exports = mongoose.model('Note', noteSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,25 @@
-
 require('dotenv').config();
-
 const express = require('express');
-const notesRoutes = require('./routes/notes');
+const mongoose = require('mongoose');
 
+const notesRouter = require('./routes/notes');
 const app = express();
 
-//Middleware 
-app.use(express.json());
 app.use((req, res, next) => {
-    console.log(`${req.path} ${req.method}`);
+    console.log(req.path, req.method);
     next();
-})
-
-app.use('/api/notes', notesRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+app.use('/api/notes', notesRouter);
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log('Server and Database are running on port', process.env.PORT);
+    })
+}).catch((error) => {
+    console.log('Error connecting to MongoDB:', error);
 });
+
+// app.listen(process.env.PORT, () => [
+//     console.log('Server is running on port 3000!!!', process.env.PORT)
+// ]);
