a44a5c51cee398205cdf77547316acdf76dec799
Elliah Panopio <elirnp@gmail.com>
2026-03-13 20:35:55 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const noteRoutes = require('./routes/notes');
+
+const app = express();
+
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+app.use('/api/notes', noteRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
