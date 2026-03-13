64674ece71e1284f4ac2f20472ddcdecbd66b414
sherbennn <gutierrezsd@students.nu-laguna.edu.ph>
2026-03-13 21:29:07 +0800

    Completed the server.js code as per activity's instructions
    Refactor server.js to include root route and fix port

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,28 @@
 require('dotenv').config();
-const express = require('express')
+
+const express = require('express');
 
 const notesRouter = require('./routes/notes');
+
 const app = express();
 
+
+app.use(express.json());
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
 
+
 app.use('/api/notes', notesRouter);
 
-app.listen(process.env.PORT, () => [
-    console.log('Server is running on port 3000!!!', process.env.PORT)
-]);
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port', process.env.PORT);
+});
+
+
