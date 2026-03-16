ee83fceb5ab73cacffc2671318ac9d2faf74374a
Adrienne Pagaran <pagaranag@students.nu-laguna.edu.ph>
2026-03-16 08:58:59 +0800

    16/3/2026 Session 3

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,27 +1,15 @@
 const express = require('express');
 const Note = require('../models/nodeModel');
 const router = express.Router();
+const {createNote} = require('../controller/noteController');
 
-
-router.get('/', (req, res) => {  
-    res.json({ 
-        message: 'Hello World! subasaskrayb 2 my yutube chanil',
-        name: 'Subas',
-        age: 30,
-        hobbies: ['coding', 'gaming', 'traveling']
-    });
-});
-
-router.post('/',async (req,res) => {
-    const{title, content} = req.body
-    try{
-        const note = await Note.create ({title, content });
-        res.status(200).json(note);
-    }
-    catch(error){
-        res.status(400).json({error: error.message});
-    }
-    res.json({message: 'Note created successfully!'});
-});
+//display
+router.get('/id', getNotesbyid); 
+//create   
+router.post('/', createNote);
+//Delete   
+router.delete('/id', deleteNoteById);
+//Update 
+router.patch('/id', updateNote);
 
 module.exports = router;
