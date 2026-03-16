018bff320d81a500149d2b36f9380471d7c2381b
ajc97488-ux <ajc97488@gmail.com>
2026-03-16 13:40:46 +0800

    updated version

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose =require('mongoose');
+
+const notesRouter = require('./routes/notes');
+const app = express();
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+app.use (express.json());
+app.use((req, res, next) => {
+   console.log(req.path, req.method);
+   next();
+});
+
+// ROUTE TO NOTES
+app.use('/api/notes', notesRouter);
+mongoose.connect(process.env.MONGO_URI).then(() =>{
+    app.listen(process.env.port, () => {
+        console.log('Server and database are running on port', process.env.port);
+    })
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
