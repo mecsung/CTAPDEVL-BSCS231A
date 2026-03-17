878c039fab70c4f5976a0ace6b0711a4b1283365
marcolc-sketch <marcolyka99@gmail.com>
2026-03-17 19:31:07 +0800

    Update noteController.js

--- Backend/controller/noteController.js
+++ Backend/controller/noteController.js
@@ -1,6 +1,20 @@
 const { default: mongoose } = require('mongoose');
 const Note = require('../models/noteModel');
 
+//Create a new note
+const createNote = async (req, res) => {
+    const { title, content } = req.body;
+
+    try {
+    const note = await Note.create({ title, content });
+    res.status(200).json(note);
+    }
+    catch (error){
+        res.status(400).json({error: error.message});
+    }
+    res.json({ message: 'Note created successfully!' });
+};
+
 //Get all notes
 const getAllNotes = async (req, res) => {
     const notes = await Note.find({}).sort({ createdAt: -1 });
@@ -20,20 +34,6 @@ const getSingleNote = async (req, res) => {
         return res.status(404).json({ error: 'Note not found' });
     }
     res.status(200).json(note);
-};
-
-//Create a new note
-const createNote = async (req, res) => {
-    const { title, content } = req.body;
-
-    try {
-    const note = await Note.create({ title, content });
-    res.status(200).json(note);
-    }
-    catch (error){
-        res.status(400).json({error: error.message});
-    }
-    res.json({ message: 'Note created successfully!' });
 };
 
 //Delete a note by ID 
