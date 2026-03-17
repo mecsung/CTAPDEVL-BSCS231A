37bb70c20684f755a47c96ba6a6f76e181c6de0c
princess-ericka <pandananprincessericka@gmail.com>
2026-03-18 00:43:34 +0800

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { createNote, getAllNote, getSingleNote, deleteNote, updateNote } = require('../controllers/noteController');
+
+const router = express.Router();
+
+// GET all notes
+router.get('/', getAllNote);
+
+// GET single note
+router.get('/:id', getSingleNote);
+
+// POST create a new note
+router.post('/', createNote);
+
+// DELETE a note
+router.delete('/:id', deleteNote);
+
+// UPDATE a note
+router.patch('/:id', updateNote);
+
+module.exports = router;
