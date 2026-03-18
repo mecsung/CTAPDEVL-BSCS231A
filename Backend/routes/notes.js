5d1fcdcf280cf3a6be93029e81ca01c894b40a00
Krisha Agojo <krishaagojo07@gmail.com>
2026-03-18 20:36:13 +0800

    Updated
    - fixed issues

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
+<<<<<<< Updated upstream
 
 router.get('/', (req, res) => {
     //    res.send('Hello World');
@@ -11,4 +12,22 @@ router.get('/', (req, res) => {
     });
 })
 
+=======
+ 
+// GET notes all
+router.get('/', getNote);
+ 
+// GET single note with ID; display
+router.get('/:id', getNote);
+ 
+// POST create a new note
+router.post('/', createNote);
+ 
+// DELETE a note by ID
+router.delete('/:id', delNote);
+ 
+// PATCH update a note
+router.patch('/:id', upNote);
+ 
+>>>>>>> Stashed changes
 module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 const notesRouter = require('./routes/notes');
 const app = express();
+const dns = require('dns');
 
 app.use((req, res, next) => {
     console.log(req.path, req.method);
@@ -11,6 +12,19 @@ app.use((req, res, next) => {
 
 app.use('/api/notes', notesRouter);
 
+<<<<<<< Updated upstream
 app.listen(process.env.PORT, () => [
     console.log('Server is running on port 3000!!!', process.env.PORT)
 ]);
+=======
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+//connect MongoDB and start the server
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log('Server and Database are running on port', process.env.PORT);
+    })
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error)
+});
+>>>>>>> Stashed changes
