25e2dfa0ce4a9273d6ba660603b6e340e2b893a3
Teddy_Carlo_010 <work.arestadoted@gmail.com>
2026-03-19 13:53:27 +0800

    Arestado Act 2
    Applied CRUD Functionality

---
+++ server/routes/note.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const controllers = require("../controllers/notesController.js");
+
+router.get("/", controllers.getAllNotes);
+router.get("/:id", controllers.getSingleNote);
+router.post("/", controllers.createNote);
+router.patch("/:id", controllers.updateNote);
+router.delete("/:id", controllers.deleteNote);
+
+module.exports = router;
