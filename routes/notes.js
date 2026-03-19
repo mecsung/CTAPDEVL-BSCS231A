99a0ea37e38b3a75ebc6136f0a9533f9f0e2f072
KeithYves08 <keithyvesrobles@gmail.com>
2026-03-19 14:00:16 +0800

    feat: model schemas and controllers

---
+++ routes/notes.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router();
+const { 
+      getNotes, 
+      getNoteWithId, 
+      deleteNote, 
+      updateNote,
+      createNote,
+   } = require("../controllers/noteController");
+
+//get all notes
+router.get("/", getNotes);
+
+//get a note with id
+router.get("/:id", getNoteWithId);
+
+//create a note
+router.post("/", createNote);
+
+//delete a note
+router.delete("/:id", deleteNote);
+
+//update a note
+router.put("/:id", updateNote);
+
+module.exports = router;
