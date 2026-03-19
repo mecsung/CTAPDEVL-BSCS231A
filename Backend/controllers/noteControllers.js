71214fe8c2abe27a81d15a4e5cb2a509b2bec22e
Caleb <calebhopemanalo@outlook.com>
2026-03-19 22:08:31 +0800

    Testing of CRUD API (POST, GET, PATCH, DELETE)
    - Added some changes to noteControllers.js by changing the const of the variables

--- Backend/controllers/noteControllers.js
+++ Backend/controllers/noteControllers.js
@@ -3,28 +3,26 @@ const Note = require('../models/noteModel.js');
 // Get all notes
 const getAllNote = async (req,res) => {
     const notes = await Note.find({}).sort({ createdAt: -1 });
-    res.status(200).json(notes);
+    res.status(200).json(note);
 };
 
-// Get single note
+// Get a single note
 const getSingleNote = async (req,res) => {
     const { _id } = req.params;
-
-    const note = await Note.findById(_id);
+    const note = await Note.findById(id);
 
     if (!note){
-        return res.status(404).json({ error: 'Note not found' });
+        return res.status(404).json({ error: 'Note not found' })
     }
-
     res.status(200).json(note);
 };
 
 // Create a new note
 const createNote = async (req, res) => {
-    const { id, title, content } = req.body;
+    const { _id, title, content } = req.body;
 
     try{
-        const note = await Note.create({ id, title, content })
+        const note = await Note.create({ _id, title, content })
         res.status(200).json(note);
     }
     catch (error){
@@ -46,7 +44,7 @@ const deleteNote = async (req,res) => {
 
 // Update a note
 const UpdateNote = async (req,res) => {
-    const { _id } = req.params;
+    const { id } = req.params;
     const { title, content } = req.body;
 
     const note = await Note.findByIdAndUpdate(_id, { title, content }, { new: true });
@@ -59,8 +57,8 @@ const UpdateNote = async (req,res) => {
 
 module.exports = {
     getAllNote,
-    getSingleNote,
     createNote,
+    getSingleNote,
     deleteNote,
     UpdateNote
 };
