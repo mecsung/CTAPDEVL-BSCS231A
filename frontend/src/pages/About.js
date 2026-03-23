a53d2c16764724345d3d49ae905c13f40d5d61dd
justhMae <maetuiza9@gmail.com>
2026-03-23 08:52:34 +0800

    frontend setup with react
    - setup pages such as Home, About, and Login
    - used react as the framework for this session

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,13 @@
+const About = () => {
+    return (
+        <div className = "about">
+            <h1>About Page</h1>
+                <h2>Vision</h2>
+                    <p>We are National University, a dynamic private institute, committed to nation building, recognized internationally in teaching and research.</p>
+                <h2>Mission</h2>
+                    <p>Guided by our core values and cultural heritage of Dynamic Filipinism, National University is committed in providing relevant,innovative, and accessible quality education and other development programs.</p>
+        </div>
+    );
+}
+
+export default About;
