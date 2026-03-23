8fac99ec17ee7a07c31805a1f9d7826bb9e971cd
princess-ericka <pandananprincessericka@gmail.com>
2026-03-23 09:01:15 +0800

    Add files via upload

---
+++ about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div className="About">
+            <h1>About Me</h1>
+            <p>This is a note system.</p>
+                
+        </div>
+    )
+}
+
+export default About;
