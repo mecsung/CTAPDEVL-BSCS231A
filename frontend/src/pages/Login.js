98fb7c58c5d80bce64e36cbefeb6e3725a9d69bc
Caleb <calebhopemanalo@outlook.com>
2026-03-23 08:55:22 +0800

    About Page

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,12 @@
+const Login = () => {
+    return (
+        <div className="login">
+            <h1>Login</h1>
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password" />
+            <button type="submit">Login</button>
+        </div>
+    )
+}
+
+export default Login;
---
+++ frontend/src/pages/about-page.js
@@ -0,0 +1,16 @@
+const About = () => {
+    return (
+        <div className="about">
+            <h1>Mission & Vission</h1>
+
+            <h2>Vission</h2>
+            <p>We are Nationational University, a dynamic private institution, commited to nation building, recognized internationally in teaching and research.</p>
+
+            <h1>Mission</h1>
+            <p>Guided by the core values and characterized by our cultural heritage of Dynamic Filipinism, nationa University is commited to prviding relevant, innovative, and accessible quality education and othe developlment programs.</p>
+            <p>we are commited to out: Students, Faculty and Employees, Alumn,  Industry partners and Employers, Community</p>
+        </div>
+    )
+}
+
+export default About;
