5ebe2f74777b1cdb9c3fd76ea4bb32626ac56067
Byron Capulong <capulongba@students.nu-laguna.edu.ph>
2026-03-23 08:56:23 +0800

    ACT 3
    - created frontend
    - made pages, components

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,29 @@
+const About = () => {
+    return (
+        <div className="about">
+            <h1>About Us</h1>
+            <h2>Shrek Script</h2>
+            <p> Once upon a time there was a lovely 
+                princess. But she had an enchantment 
+                upon her of a fearful sort which could 
+                only be broken by love's first kiss. 
+                She was locked away in a castle guarded 
+                by a terrible fire-breathing dragon. 
+                Many brave knights had attempted to 
+                free her from this dreadful prison, 
+                but non prevailed. She waited in the 
+                dragon's keep in the highest room of 
+                the tallest tower for her true love 
+                and true love's first kiss. (laughs) 
+                Like that's ever gonna happen. What 
+                a load of - (toilet flush)
+ 
+               Allstar - by Smashmouth begins to play. Shrek goes about his 
+               day. While in a nearby town, the villagers get together to go 
+               after the ogre.</p>
+            <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGxldmIweWU5MjN2N3dmd2p4em1scjF2cmQ5MG4xamR1aXFkNHF3eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/l41YeEfEJyDbGIuwE/giphy.gif"></img>
+        </div>
+    )
+}
+
+export default About;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>whats good holmes</h1>
+            <button>touch me</button>
+        </div>
+    )
+}
+
+export default Home;
