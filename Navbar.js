5f547204048200e03d66e7bb501ec6c1d1e5d750
Elliah Panopio <elirnp@gmail.com>
2026-03-23 09:01:13 +0800

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div className="container">
+            <h1> My app </h1>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/About">About</Link></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
