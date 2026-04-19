c684e01e8d3db3c4fb3c73c970c3fc8651bbddc2
justhMae <maetuiza9@gmail.com>
2026-04-20 07:33:35 +0800

    nice
    nice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/AboutUs";
 import Login from "./pages/Login";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 
 const App = () => {
@@ -21,7 +21,7 @@ const App = () => {
           </Routes>
         </main>
 
-        <Footer />
+        <Footer />        import Navbar from './components/Navbar';
       </div>
     </BrowserRouter>
   );
