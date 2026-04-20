82a01d237cb345b69173ee74186c0d3681c0d27e
azecastillo <azecastillo777@gmail.com>
2026-04-20 08:37:39 +0800

    act5-ReactContext
    -Added auto update for notes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,34 @@
+import { useEffect } from "react";
+import { useNotesContext } from "../hooks/useNotesContext";
+
+import NoteData from "../components/NoteData";
+import NoteForm from "../components/NoteForm";
+
 const Home = () => {
+    const { notes, dispatch } = useNotesContext(); 
+
+    useEffect(() => {
+        const fetchNotes = async () => {
+            const response = await fetch("/api/notes");
+            const json = await response.json();
+
+            if (response.ok) {
+                dispatch({ type: 'SET_NOTES', payload: json });
+            }
+        };
+        fetchNotes();
+    }, [dispatch]); 
     return (
         <div className="home">
             <h1>Welcome to Notes System</h1>
             <p>Your modern, secure, and easy-to-use note-taking platform for National University students.</p>
+            <NoteForm />
+
+            <div className="notes">
+                {notes && notes.map((note) => (
+                    <NoteData key={note._id} note={note} />
+                ))}
+            </div>
         </div>
     );
 };
