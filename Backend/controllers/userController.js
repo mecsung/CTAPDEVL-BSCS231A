9cd7011e9ce369c1cf83eca211319ce5c6026d0d
calebskiii <calebhopemanalo@outlook.com>
2026-05-14 09:02:15 +0800

    User/Login Function

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -5,8 +5,17 @@ const createToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
 }
 
-const loginUser = (req, res) => {
-    res.json({ message: 'Login successful' });
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        const user = await User.login(email, password);
+
+        const token = createToken(user._id);
+        res.status(200).json({email, token})
+    } catch (error){
+        res.status(400).json({ error: error.message})
+    }
 };
 
 const signupUser = async (req, res) => {
